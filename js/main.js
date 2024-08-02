0f407905b6db187e1210e79fee6ff5d12eaa7304
Raj Patel <1712001rajpatel@gmail.com>
2024-08-02 10:26:49 -0400

    back to top code added and links

--- js/main.js
+++ js/main.js
@@ -78,27 +78,27 @@ itemsTest.forEach((el) => {
     }
 })
 
-// // back to top
-// document.addEventListener('DOMContentLoaded', () => {
-//     const backToTopButton = document.getElementById('backToTop');
+// back to top
+document.addEventListener('DOMContentLoaded', () => {
+    const backToTopButton = document.getElementById('backToTop');
 
-//     // Show the button when scrolling down
-//     window.addEventListener('scroll', () => {
-//         if (window.scrollY > 300) { // Show button after scrolling down 300px
-//             backToTopButton.style.display = 'block';
-//         } else {
-//             backToTopButton.style.display = 'none';
-//         }
-//     });
+    // Show the button when scrolling down
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 300) { // Show button after scrolling down 300px
+            backToTopButton.style.display = 'block';
+        } else {
+            backToTopButton.style.display = 'none';
+        }
+    });
 
-//     // Scroll to top when button is clicked
-//     backToTopButton.addEventListener('click', () => {
-//         window.scrollTo({
-//             top: 0,
-//             behavior: 'smooth'
-//         });
-//     });
-// });
+    // Scroll to top when button is clicked
+    backToTopButton.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+});
 
 let itemstopSellSlide = document.querySelectorAll('.carousel .carousel-item.topSellSlide')
 
